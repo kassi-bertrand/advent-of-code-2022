47f57caf9d6a64b923d0d8f31ac76f45d82d01c8
kassi-bertrand <bertran.billy@gmail.com>
2022-12-04 18:09:49 -0600

    Introducing a module file to store functions I use a lot

---
+++ module.py
@@ -0,0 +1,15 @@
+#I use this file as a module to store
+#functions I consistently use across
+#AoC challenge
+
+
+"""
+param:      filename    -> The path to the relevant file
+return:     lines       ->
+Description:            -> Reads a specified file and returns
+                        the lines in the file
+"""
+def readFile(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    return lines
