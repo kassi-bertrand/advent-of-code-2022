061b182b5c81b8bbbc95ea16dcda64c8a2dd80bf
kassi-bertrand <bertran.billy@gmail.com>
2022-12-11 18:10:32 -0600

    Day 5 solution

---
+++ day-5/part-1.py
@@ -0,0 +1,77 @@
+#NOTE:  The "input.txt" file contains the original input
+#       Reading and parsing directly "input.txt" is non-trivial
+#       So, I handled the input file by:
+#       1- Hard coding the initial state of the stacks
+#       2- Leaving rearrangement  procedure in a seperate file "procedure.txt"
+
+import re
+
+#read file and return the lines in it
+def readFile(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close()
+    return lines
+
+"""
+Uses "lines" and return all procedure as a list of lists (of integer)
+Where each list represents a SINGLE move
+Each list is of the form [x, y, z] where:
+    x   -> # of crates to be moved
+    y   -> src stack
+    z   -> dst stack
+"""
+def procedureList(lines):
+    procedure = []
+    regex = "([0-9]+)"
+    
+    for line in lines:
+        list = re.findall(regex, line) #Match all numbers in "line" using regex
+        for i, element in enumerate(list):
+            list[i] =  int(element)
+        #add list of integers to procedure
+        procedure.append(list)
+    
+    return procedure
+
+"""
+Move a group of crates from one stack to another
+"""
+def moveCrates(stacks, crateNumber, srcStack, dstStack):
+    #Cannot move more crates than there already is.
+    if len(stacks[srcStack]) < crateNumber:
+        return
+
+    #Move crate(s) one at a time
+    for _ in range(crateNumber):
+        poppedCrate = stacks[srcStack].pop()
+        stacks[dstStack].append(poppedCrate)
+
+if __name__ == "__main__":
+    
+    #Initial stack of crates from the "input.txt"
+    stacks = [
+        [],
+        ['V', 'C', 'D', 'R', 'Z', 'G', 'B', 'W'],
+        ['G', 'W', 'F', 'C', 'B', 'S', 'T', 'V'],
+        ['C', 'B', 'S', 'N', 'W'],
+        ['Q', 'G', 'M', 'N', 'J', 'V', 'C', 'P'],
+        ['T', 'S', 'L', 'F', 'D', 'H', 'B'],
+        ['J', 'V', 'T', 'W', 'M', 'N'],
+        ['P', 'F', 'L', 'C', 'S', 'T', 'G'],
+        ['B', 'D', 'Z'],
+        ['M', 'N', 'Z', 'W']
+    ]
+
+    #Read in the list of individual moves
+    lines = readFile('procedure.txt')
+    procedures = procedureList(lines)
+
+    #Move the crates around
+    for procedure in procedures:
+        moveCrates(stacks, procedure[0], procedure[1], procedure[2])
+    
+    #Gather elements on top of each stack and print them
+    for index in range(1, len(stacks)):
+        currList = stacks[index]
+        print(currList[len(currList) - 1], end="") #Expected TBVFVDZPN
